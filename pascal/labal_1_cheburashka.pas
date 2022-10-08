3070eb3eebb47d6b49f1763b63d6c8382b34a412
User <User@DESKTOP-DTOGNFN>
2022-10-08 14:36:10 +0300

    [pascal] Laba #1 - Finish

--- pascal/labal_1_cheburashka.pas
+++ pascal/labal_1_cheburashka.pas
@@ -8,8 +8,7 @@ begin
   head_y := 160;
   Circle(320,160,50);
   FloodFill(320, 160, clKhaki);
-  
-  
+    
   // EYE (Глаз Л)
   var eye_l_x, eye_l_y: integer;
   eye_l_x := 300;
@@ -30,12 +29,13 @@ begin
   nose_x2 := 325;
   nose_y1 := 160;
   nose_y2 := 180;
-   (nose_x1, nose_y1, nose_x2, nose_y2);
+  Ellipse(nose_x1, nose_y1, nose_x2, nose_y2);
+  FloodFill(320, 170, clYellow);
   
   // Mouth
   var mouth_x1, mouth_x2, mouth_y1, mouth_y2: integer;
   mouth_x1 := 310;
-  mouth_x2 := 330;
+  mouth_x2 := 332;
   mouth_y1 := 190;
   mouth_y2 := 190;
   Line(mouth_x1, mouth_y1, mouth_x2, mouth_y2);
@@ -47,6 +47,7 @@ begin
   ear_L_y1 := 90;
   ear_L_y2 := 220;
   Ellipse(ear_L_x1, ear_L_y1, ear_L_x2, ear_L_y2);
+  FloodFill(250, 100, clBrown);
   
   // EAR_R (Ухо П)  
   var ear_R_x1, ear_R_x2, ear_R_y1, ear_R_y2: integer;
@@ -55,6 +56,7 @@ begin
   ear_R_y1 := 90;
   ear_R_y2 := 220;
   Ellipse(ear_R_x1, ear_R_y1, ear_R_x2, ear_R_y2);
+  FloodFill(390, 110, clBrown);
   
   // Body (Тело)  
   var body_x1, body_x2, body_y1, body_y2: integer;
@@ -63,7 +65,7 @@ begin
   body_y1 := 210;
   body_y2 := 360;
   Ellipse(body_x1, body_y1, body_x2, body_y2);
-  floodfill(300, 300, clBrown);
+  FloodFill(300, 300, clBrown);
   
   // Arm_L (Рука Л)  
   var arm_L_x1, arm_L_x2, arm_L_y1, arm_L_y2: integer;
@@ -72,6 +74,7 @@ begin
   arm_L_y1 := 260;
   arm_L_y2 := 270;
   Ellipse(arm_L_x1, arm_L_y1, arm_L_x2, arm_L_y2);
+  FloodFill(240, 265, clBrown);
   
   // Arm_R (Рука П)  
   var arm_R_x1, arm_R_x2, arm_R_y1, arm_R_y2: integer;
@@ -80,6 +83,7 @@ begin
   arm_R_y1 := 260;
   arm_R_y2 := 270;
   Ellipse(arm_R_x1, arm_R_y1, arm_R_x2, arm_R_y2);
+  FloodFill(370, 265, clBrown);
   
   // Foot_L (Нога Л)  
   var foot_L_x1, foot_L_x2, foot_L_y1, foot_L_y2: integer;
@@ -88,6 +92,7 @@ begin
   foot_L_y1 := 345;
   foot_L_y2 := 365; 
   Ellipse(foot_L_x1, foot_L_y1, foot_L_x2, foot_L_y2);
+  FloodFill(270, 350, clBrown);
   
   // Foot_L (Нога П)  
   var foot_R_x1, foot_R_x2, foot_R_y1, foot_R_y2: integer;
@@ -96,14 +101,6 @@ begin
   foot_R_y1 := 345;
   foot_R_y2 := 365; 
   Ellipse(foot_R_x1, foot_R_y1, foot_R_x2, foot_R_y2);
-  
-  // Foot_L (Нога П)  
-//  var foot_R_x1, foot_R_x2, foot_R_y1, foot_R_y2: integer;
-//  foot_R_x1 := 230;
-//  foot_R_x2 := 282;
-//  foot_R_y1 := 260;
-//  foot_R_y2 := 270;
-//  Ellipse(foot_L_x1, foot_L_y1, foot_L_x2, foot_L_y2);
-  
+  FloodFill(350, 350, clBrown);
   
 end.
