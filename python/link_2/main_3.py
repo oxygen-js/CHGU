0437031faf03aa13a7a1f1c07b4f3a18eace7f36
oxygen-js <89063820142ka@gmail.com>
2022-10-11 21:46:44 +0300

    [python] Laba 2 - Task #3

--- python/link_2/main_3.py
+++ python/link_2/main_3.py
@@ -3,10 +3,14 @@ b = int(input('b: '))
 c = int(input('c: '))
 d = int(input('d: '))
 
-
+head = ""
 for i in range(a, b):
-    print('\t', i)
+    head += '\t' + str(i)
+
+print(head)
 
 for i in range(c, d):
+    stroked = ""
     for j in range(a, b):
-        print(i, '\t', i * j)
+        stroked += '\t' + str(i * j)
+    print(i, stroked)
