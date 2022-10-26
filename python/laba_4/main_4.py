db4341b11dd0a106be1171f995598ee6b0c07894
a.kazakov <a.kazakov@bidzaar.com>
2022-10-26 18:17:29 +0300

    [python] Laba #4 - Task #4

--- python/laba_4/main_4.py
+++ python/laba_4/main_4.py
@@ -6,7 +6,12 @@ class Game:
         self.command1 = {"name": club_1, "goal": randint(0, 15)}
         self.command2 = {"name": club_2, "goal": randint(0, 15)}
 
-    
+
+class Table:
+    dict
+    # ВОЗМОЖНО ВСТАВИТЬ НАЗВАНИЯ КОМАНД?
+    # def __init__(self, club, all_game, win, draw, loss):
+        # self.club["name"] = { "name": club["name"], "all_game": all_game, "win": win, "draw": draw, "loss": loss }
 
 
 all_game = int(input("All game: "))
@@ -30,4 +35,21 @@ for i in range(all_game):
 
 
 for i in games:
-    print(i.command1, i.command2)
+    print(f"{i.command1['name']} - {i.command1['goal']}; {i.command2['name']} - {i.command2['goal']}")
+
+
+#Команда:Всего_игр Побед Ничьих Поражений Всего_очков
+table = Table() 
+for i in games:
+    name_club_1 = i.command1["name"]
+    name_club_2 = i.command2["name"]
+    table[name_club_1] = name_club_1
+    table[name_club_2] = name_club_2
+    if i.command1["goal"] > i.command2["goal"]:
+        table[name_club_1]["win"] = table[name_club_1]["win"] + 1
+    else:
+        table[name_club_2]["win"] = table[name_club_2]["win"] + 1
+
+    
+
+print(table)
