f4354d2f4c2d50ba10314929bb032ce3e9754c1a
a.kazakov <a.kazakov@bidzaar.com>
2022-11-16 17:38:56 +0300

    [cpp] Laba #6

--- c++/laba_6/main.cpp
+++ c++/laba_6/main.cpp
@@ -1,81 +1,85 @@
 #include <iostream>
 using namespace std;
 
-class D4
+class B1
 {
-public:
-    double x;
-    D4() {}
-    D4(double in_x)
-    {
-        x = in_x;
-    }
+    int a;
 
-    void show()
+public:
+    B1() {}
+    B1(int in_a)
     {
-        cout << "D4.X: " << x << endl;
+        a = in_a;
+    }
+    void show_B1() { cout << "B1: " << a << endl; }
+};
+
+class D1 : public B1
+{
+    int b;
+
+public:
+    D1(int x, int y) : B1(y) { b = x; }
+    void show_D1()
+    {
+        cout << "D1: " << b << endl;
+        show_B1();
+    }
+};
+
+class D2 : private B1
+{
+    int c;
+
+public:
+    D2(int x, int y) : B1(y) { c = x; }
+    void show_D2()
+    {
+        cout << "D2: " << c << endl;
+        show_B1();
+    }
+};
+
+class D4 : public D1, public D2
+{
+    int d;
+
+public:
+    D4(int x, int y, int z, int n, int e) : D1(y, z), D2(n, e) { d = x; }
+    void show_D4()
+    {
+        cout << "D4: " << d << endl;
+        show_D1();
+        show_D2();
     }
 };
 
 class B2 : private D4
 {
+    int e;
+
 public:
-    double y;
-    B2(double in_y)
+    B2(int x, int y, int z, int n, int e, int l) : D4(y, z, n, e, l) { e = x; }
+    void show_B2()
     {
-        y = in_y;
-    }
-    void show()
-    {
-        cout << "B2.Y: " << y << endl;
+        cout << "B2: " << e << endl;
+        show_D4();
     }
 };
-
-class D2 : public D4
-{
-public:
-    D2() {}
-    D2(double in_x)
-    {
-        x = in_x;
-    }
-};
-
-class D1 : public D4
-{
-public:
-    D1() {}
-    D1(double in_x)
-    {
-        x = in_x;
-    }
-};
-
-// class B1 : public D1, private D2
-// {
-// public:
-//     B1(double in_x)
-//     {
-//         x = in_x;
-//     }
-// };
 
 int main()
 {
-    D4 obj_d4 = D4(1);
-    obj_d4.show();
+    B2 temp(100, 200, 300, 400, 500, 600);
+    D4 temp1(10, 20, 30, 40, 50);
 
-    D1 obj_d1 = D1(2);
-    obj_d1.show();
+    cout << "B2 temp(100, 200, 300, 400, 500, 600);\n";
+    cout << "D4 temp(10, 20, 30, 40, 50);\n";
 
-    D2 obj_d2 = D2(3);
-    obj_d2.show();
+    cout << "\nСледуя иерархии класса B2: \n";
+    temp.show_B2();
 
-    B2 obj_b2 = B2(4);
-    obj_b2.show();
-
-
-    // B1 obj_b1 = B1(5);
+    cout << "\nСледуя иерархии класса D4: \n";
+    temp1.show_D4();
 
     return 0;
 }
