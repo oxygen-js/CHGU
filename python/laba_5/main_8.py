c28d0fb3f7681a42b8f3cc94918ede3b11405ed5
a.kazakov <a.kazakov@bidzaar.com>
2022-11-16 22:53:04 +0300

    [python] Laba#5 - Task #8

---
+++ python/laba_5/main_8.py
@@ -0,0 +1,34 @@
+def generate(fio, group, ocenka):
+    return [fio, group, sum(ocenka) / len(ocenka)]
+
+
+def get_bad_student(item):
+    if (item[2] < 3):
+        return [item[0], item[1]]
+
+
+fio = ["Kazakov Ivan Gennadievich", "Petrov Sasha Michalovich", "Suslikova Sveta Andreevna", "Ivanov Ivan Andreevich", "Sidorov Andrey Gennadievich",
+       "Tigrova Anna Radionovna", "Lampova Irina Olegovna", "Popov Andrey Gennadievich", "Tabulanov Oleg Petrovich", "Mokeev Andrey Aleksandrovich"]
+
+groups = ["КТ-41-29", "КТ-52-19", "КТ-65-17", "КТ-42-34", "КТ-85-29",
+          "КТ-44-73", "КТ-10-007", "КТ-13-13", "КТ-12-11", "КТ-25-99"]
+
+ocenka = [
+    [5.4, 4.4, 5.5, 3.0, 4.2],
+    [2.2, 2.4, 2.4, 2.3, 2.2],
+    [3.2, 3.3, 3.4, 2.2, 3.2],
+    [5.3, 4.4, 5.4, 4.5, 5.2],
+    [3.3, 3.4, 4.4, 5.1, 5.2],
+    [4.3, 3.4, 5.2, 5.2, 3.1],
+    [4.3, 4.4, 4.2, 2.2, 5.1],
+    [3.3, 3.4, 3.3, 5.0, 3.1],
+    [4.0, 3.0, 3.3, 5.5, 4.1],
+    [4.5, 3.5, 3.5, 5.5, 5.5],
+]
+
+students = list(filter(lambda x: x, map(get_bad_student, map(generate, fio, groups, ocenka))))
+
+for item in students:
+    short_name = item[0].split(' ')
+    name = f"{short_name[0]} {short_name[1][0:1]}.{short_name[2][0:1]}."
+    print(f"{name} - {item[1]}")
