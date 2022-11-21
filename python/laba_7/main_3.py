87771209561a53cb66e44e8da9f188088df3a121
a.kazakov <a.kazakov@bidzaar.com>
2022-11-21 23:17:47 +0300

    [python] Laba #7 - Task #3

--- python/laba_7/main_3.py
+++ python/laba_7/main_3.py
@@ -1,5 +1,5 @@
 import csv
-from operator import attrgetter, itemgetter
+from operator import itemgetter
 
 
 def is_digit(string):
@@ -13,15 +13,36 @@ def is_digit(string):
             return False
 
 
+def print_table(table):
+    head = "{0:<35}|{1:^6}|{2:^9}|{3:^7}|{4:^8}|{5:^9} |".format(*list(map(lambda x: x.split(',')[0], table[0])))
+    print(head, "\n", "-" * len(head))
+    for i in table[1:]:
+        values = [v for k, v in i.items()]
+        print("{0:<35}|{1:^6.0f}|{2:>8.2f} |{3:>6} |{4:>7.1f} |{5:>9.1f} |".format(*values))
+
+
+def sorted_table(data, revers=False, *key):
+    result = sorted(data, key=itemgetter(*key), reverse=revers)
+    return result
+
+
 with open("movies.csv") as csv_file:
     reader = csv.DictReader(csv_file)
     moves = []
     for move in list(reader):
-        for k, val in move.items():
+        cp_move = move.copy()
+        for k, val in cp_move.items():
             if is_digit(val):
                 move[k] = float(val)
+            if k == "Жанр":
+                del move[k]
         moves.append(move)
 
-# создать класс и на его основе сделать объекты по ключам -> использовать метод atrgetter из методички
-    for i in sorted(moves, key=itemgetter(3, 6), reverse=True):
-        print(i)
+print("TABLE #1 - Сортировка по Бюджету и Сборам")
+table_sort_bug_fees = sorted_table(moves, False, "Бюджет, \\$ млн", "Сборы, \\$ млн")
+print_table(table_sort_bug_fees)
+
+print("\n\nTABLE #2 - Сортировка по рейтингу в порядке убывания")
+table_sort_rating = sorted_table(moves, True, "Рейтинг")
+print_table(table_sort_rating)
+
