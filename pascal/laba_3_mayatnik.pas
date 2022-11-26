fe0096692652c6ce5f0dfd2088474843a74384ae
Ojo <kazak1995kazak@yandex.ru>
2022-11-26 12:47:32 +0300

    [pascal] Laba #3

---
+++ pascal/laba_3_mayatnik.pas
@@ -0,0 +1,31 @@
+﻿uses crt,graphabc;
+var x,y:integer; i : real;
+begin
+hidecursor;
+repeat
+repeat
+         i:=i+pi/100;
+         x:=round(100*(-cos(i)));
+         y:=round(10*(sin(i)));
+         lockdrawing;
+         ClearWindow;
+         line(250,0,x+250,y+250);
+         circle(x+250,y+250,50);
+         redraw;
+         sleep(10);
+until i >= pi;
+i:=0;
+repeat
+         i:=i+pi/100;
+         x:=round(100*(cos(i)));
+         y:=round(10*(sin(i)));
+         lockdrawing;
+         ClearWindow;
+         line(250,0,x+250,y+250);
+         circle(x+250,y+250,50);
+         redraw;
+         sleep(10);
+until i >= pi;
+If i >= pi then i := 0;
+until keypressed;
+end.
