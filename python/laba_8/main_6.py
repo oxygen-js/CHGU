7880631cabd1994bf13cc6733313d2ed36c428f2
oxygen-js <89063820142ka@gmail.com>
2022-12-14 11:14:14 +0300

    [c++] Laba_8 and other

--- python/laba_8/main_6.py
+++ python/laba_8/main_6.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def np_encode(a):
     #elems = np.squeeze(a).shape
-    print(a.ndim)
+    print(a.shape)
 
 
 X = np.array([1, 2, 2, 3, 3, 1, 1, 5, 5, 2, 3, 3])
---
+++ python/laba_9/main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+df = pd.read_csv("C:\Users\Kazak\Desktop\codes\×ÃÓ\CH_GU\python\laba_9\bike.csv") 
+df
