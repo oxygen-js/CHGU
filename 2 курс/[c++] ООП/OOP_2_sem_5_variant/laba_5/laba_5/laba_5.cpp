8aeca6c0ec0b4d1e6ad39ba5fa6d76f39959fb3a
oxygen-js <89063820142ka@gmail.com>
2023-04-18 13:16:13 +0300

    [c++] Laba #5

--- 2 курс/[c++] ООП/OOP_2_sem_5_variant/laba_5/laba_5/laba_5.cpp
+++ 2 курс/[c++] ООП/OOP_2_sem_5_variant/laba_5/laba_5/laba_5.cpp
@@ -1,63 +1,64 @@
 ﻿#include <iostream>
+#include <string>
 
 
-void generate(int* arr, int arr_size, int fill_num)
+void print_header(const int title, const int variant, const std::string task) 
+{
+	std::cout << "LABA #" << title << ". Variant #" << variant << std::endl;
+	std::cout << "TASK:\n" << task << '\n' << std::endl;
+}
+
+
+void print(int* arr, int arr_size)
 {
 	for (int i = 0; i < arr_size; i++)
-	{
-		arr[i] = fill_num;
-	}
+		std::cout << arr[i] << ", ";
+	std::cout << std::endl;
 }
 
 
 int main()
 {
-	int n, m;
-	std::cin >> n;
-	std::cin >> m;
+	setlocale(LC_ALL, "Russian");
+	print_header(5, 5, "Даны натуральные числа n, m и массивы a и b одного типа из n и m элементов соответственно. Получить массив c из n+m элементов, чередуя элементы массивов a и b. Оставшиеся элементы наиболее длинного массива разместить в конце массива c.");
 
-	int max_size = (n > m) ? n : m;
-	int min_size = (n > m) ? m : n;
+	int n = 0;
+	int m = 0;
+	std::cout << "N: "; std::cin >> n;
+	std::cout << "M: ";	std::cin >> m;
+	std::cout << std::endl;
 
+	int c_size = n + m;
 	int* a = new int(n);
 	int* b = new int(m);
+	int* c = new int(c_size);
 	int* ptr_max_arr = n > m ? a : b;
-	int* c = new int(max_size);
 
-	for (int i = 0; i < n; i++)
+	std::cout << "A: ";
+	for (int i = 0; i < n; i++) a[i] = 1;
+	print(a, n);
+
+	std::cout << "B: ";
+	for (int i = 0; i < m; i++) b[i] = 2;
+	print(b, m);
+
+	
+	std::cout << "C: ";
+	int it_a = 0;
+	int it_b = 0;
+	for (int i = 0; i < c_size;i)
 	{
-		a[i] = 1;
+		if (it_a < n) {
+			c[i] = a[it_a++];
+			i++;
+		}
+
+		if (it_b < m) {
+			c[i] = b[it_b++];
+			i++;
+		}
 	}
-
-	for (int i = 0; i < m; i++)
-	{
-		b[i] = 2;
-	}
-
-	for (int i = 0; i < min_size; ++i)
-	{
-		c[i] = a[i];
-		c[i] = b[i];
-	}
-
-	for (int i = min_size; i < max_size; i++)
-	{
-		c[i] = ptr_max_arr[i];
-	}
-
-
-	for (int i = 0; i < max_size; i++)
-	{
-		std::cout << c[i] << ", ";
-	}
-
+	print(c, c_size);
 
 	return 0;
 }
-
-/*
-* ЗАДАЧА. Вариант №5:
-* Даны натуральные числа n, m и массивы a и b одного типа из n и m элементов соответственно.
-* Получить массив c из n+m элементов, чередуя элементы массивов a и b.
-* Оставшиеся элементы наиболее длинного массива разместить в конце массива c.
-*/
