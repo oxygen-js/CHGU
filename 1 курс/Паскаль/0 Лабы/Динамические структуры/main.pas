d4e837c8b15558026ab70434b01c41cf754e1ea4
oxygen-js <89063820142ka@gmail.com>
2023-05-05 11:06:18 +0300

    [pascal] Labels

---
+++ 1 курс/Паскаль/0 Лабы/Динамические структуры/main.pas
@@ -0,0 +1,115 @@
+﻿//Динамические структуры. 7 вариант
+
+const
+  p = 2;//поступление через каждые
+  t = 10;//время на проверку одного изделия
+  T0 = 40;//общее время
+  m = 3;//кол-во контроллеров
+
+type
+  ptr = ^Detal;
+  Detal = record
+    number: integer;
+    next: ptr;
+  end;
+
+type
+  DataControl = record
+    free: boolean;
+    time: integer;
+  end;
+
+var
+  Controller: array of DataControl;
+  Fproduct, Lproduct: ptr;
+  i, j, num: integer;
+
+
+procedure creat(var Fproduct: ptr; var Lproduct: ptr; n: integer);
+var
+  product: ptr;
+begin
+  if Fproduct = nil then begin
+    new(product);
+    product^.number := n;
+    product^.next := nil;
+    Fproduct := product;
+    Lproduct := product; end
+  else begin
+    new(product);
+    product^.number := n;
+    product^.next := nil;
+    Lproduct^.next := product;
+    Lproduct := product; end;
+end;
+
+
+function count(Fproduct: ptr): integer;
+var
+  product: ptr;
+  q: integer;
+begin
+  product := Fproduct;
+  q := 0;
+  while product <> nil do
+  begin
+    q := q + 1;
+    product := product^.next; end;
+  count := q;
+end;
+
+procedure print(Fproduct: ptr);
+var
+  product: ptr;
+begin
+  product := Fproduct;
+  while product <> nil do
+  begin
+    write(product^.number, ' ');
+    product := product^.next; end;
+end;
+
+
+procedure delete(var Fproduct: ptr);
+var
+  product: ptr;
+begin
+  product := Fproduct;
+  Fproduct := product^.next;
+  dispose(product);
+end;
+////////////////////////////////////////////////////////////////////////////////
+begin
+  Fproduct := nil;
+  Lproduct := nil;
+  num := 0;
+  
+  setLength(Controller, m);
+  for i := 0 to (Length(Controller) - 1) do
+  begin
+    Controller[i].free := false;
+    Controller[i].time := 0; end;
+  
+  for i := 1 to T0 do
+  begin
+    if (i mod p = 0) then begin
+      num := num + 1;
+      creat(Fproduct, Lproduct, num); end;
+    if count(Fproduct) > 0 then
+      for j := 0 to (Length(Controller) - 1) do
+      begin
+        if Controller[j].free = false then begin
+          Controller[j].free := true;
+          Controller[j].time := t + 1;
+          delete(Fproduct);
+          break; end;
+      end;
+    for j := 0 to Length(Controller) - 1 do
+    begin
+      if Controller[j].time <> 0 then
+        Controller[j].time := Controller[j].time - 1
+      else begin
+        Controller[j].free := false; end; end;
+  end;
+  write('Через ', T0, ' минут в накопителе будет находиться деталь №: '); print(Fproduct);
+end.
---
+++ 1 курс/Паскаль/0 Лабы/Длинная целочисленная арифметика/main.pas
@@ -0,0 +1,159 @@
+﻿// Вариант #7.	Составить программу вычисления числа размещений из N элементов по M
+
+{ Выравнивание длин чисел; }
+procedure Equel(var s1, s2: string; var l: integer);
+var
+  i, x1, x2: integer;
+begin
+  x1 := Length(s1);
+  x2 := Length(s2);
+  if x1 > x2 then begin
+    l := x1;
+    for i := 1 to l - x2 do 
+      s2 := '0' + s2
+  end
+  else begin
+    l := x2;
+    for i := 1 to l - x1 do 
+      s1 := '0' + s1
+  end
+end;
+
+
+{ Сложение; }
+function Plus(a, b: string): string;
+var
+  i, k, L: integer;
+  x, x1, x2: integer;
+  s, c: string;
+  p: boolean;
+begin
+  Equel(a, b, L);
+  p := False;
+  s := '';
+  for i := L Downto 1 do
+  begin
+    Val(a[i], x1, k);
+    Val(b[i], x2, k);
+    x := x1 + x2; 
+    if p then 
+      x := x + 1;
+    if x > 9 then 
+      p := True 
+    else 
+      p := False;
+    x := x mod 10;
+    Str(x, c);
+    s := c + s
+  end;
+  if p then 
+    s := '1' + s;
+  Plus := s
+end;
+
+
+{ Вычитание; }
+function Minus(a, b: string): string;
+var
+  i, k, L: integer;
+  x, x1, x2: integer;
+  s, c: string;
+  p: boolean;
+begin
+  Equel(a, b, L);
+  p := False;
+  s := '';
+  for i := L Downto 1 do
+  begin
+    Val(a[i], x1, k);
+    Val(b[i], x2, k);
+    if p then 
+      x1 := x1 - 1;
+    if x1 < x2 then 
+    begin 
+      x1 := x1 + 10; 
+      p := True 
+    end
+    else 
+      p := False;
+    x := x1 - x2;
+    Str(x, c);
+    s := c + s
+  end;
+  while (s[1] = '0') and (Length(s) > 1) do 
+    Delete(s, 1, 1);
+  Minus := s
+end;
+
+
+{ Умножение числа на одну цифру; }
+function MultiOne(a: string; b: integer): string;
+var
+  i, j: integer;
+  s, s1: string;
+  x: integer;
+begin
+  s := '';
+  for i := Length(a) Downto 1 do
+  begin
+    x := StrToInt(a[i]);
+    x := x * b;
+    s1 := IntToStr(x);
+    for j := 1 to (Length(a) - i) do 
+      s1 := s1 + '0';
+    s := Plus(s, s1)
+  end;
+  MultiOne := s
+end;
+
+
+{ Перемножение; }
+function Multi(a, b: string): string;
+var
+  i, j: integer;
+  s1, s: string;
+begin
+  s := '';
+  for i := Length(b) Downto 1 do
+  begin
+    s1 := MultiOne(a, StrToInt(b[i]));
+    for j := 1 to (Length(b) - i) do 
+      s1 := s1 + '0';
+    s := Plus(s, s1)
+  end;
+  Multi := s
+end;
+
+
+
+function Fact(N: string): string;
+var
+  s, c: string;
+begin
+  s := '1';
+  c := '0';
+  repeat
+    c := Plus(c, '1');
+    s := Multi(s, c)
+  until c = N;
+  Fact := s;
+end;
+
+
+var
+  N, M, c, a, b, s: string;
+
+begin
+  write('N = '); readln(N);
+  write('M = '); readln(M);
+  s := '1';
+  c := '0';
+  repeat
+    c := Plus(c, '1');
+    a := Minus(N, c);
+    b := Plus(a, '1');
+    s := Multi(s, b);
+  until c = M;
+
+  writeln('Ответ = ', s);
+end.
